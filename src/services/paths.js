dc42f090d7803a1981ae14af779bd85bf52b57a4
Víctor Manuel Hernández Hernández <victor.hernandez@gmstatecla.alcaldia>
2022-09-09 10:43:37 -0600

    Server

--- src/services/paths.js
+++ src/services/paths.js
@@ -2,15 +2,15 @@
 const url_base = process.env.VUE_APP_URL_BASE
 
 // Routes Community
-export const PortadaInicio = () => `${url_base}/api/portada-inicio/`
+export const PortadaInicio = () => `${url_base}/api/portada-inicio`
 
 export const PortadaRegistro = () => `${url_base}/api/portada-registro`
 
-export const Tag = () => `${url_base}/api/tag/`
+export const Tag = () => `${url_base}/api/tag`
 
 export const Categoria = () => `${url_base}/api/categoria/`
 
-export const CategoriaDestacado = () => `${url_base}/api/categoria-destacado/`
+export const CategoriaDestacado = () => `${url_base}/api/categoria-destacado`
 
 export const CategoriaClick = () => `${url_base}/api/categoria-click/`
 
@@ -18,6 +18,6 @@ export const CatalogoCategoria = () => `${url_base}/api/catalogo/`
 
 export const Cuenta = () => `${url_base}/api/cuenta/`
 
-export const RegistroServicio = () => `${url_base}/api/registro-servicio/`
+export const RegistroServicio = () => `${url_base}/api/registro-servicio`
 
-export const RegistroEmpresa = () => `${url_base}/api/registro-empresa/`
+export const RegistroEmpresa = () => `${url_base}/api/registro-empresa`
